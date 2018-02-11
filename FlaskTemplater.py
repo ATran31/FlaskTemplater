15cb0d05ca37c88e658d8bc8b15aee2f341cc226
An Tran <an.tran@dci.com>
2018-02-10 21:46:18 -0500

    init commit.

---
+++ FlaskTemplater.py
@@ -0,0 +1,175 @@
+# Creates a Flask project in a user specified folder.
+# It does not create a virtualenv or install any dependencies.
+# The folder structure is as follows:
+# ./
+# ./run.py
+# ./app_pkg/
+# ____./templates/
+# ________./index.html
+# ____./static/
+# ________./css/
+# ____________./app.css
+# ________./js/
+# ____________./app.js
+# ____./__init__.py
+# ____./config.py
+# ____./forms.py
+# ____./models.py
+# ____./views.py
+
+import os
+from sys import argv
+
+
+class FlaskTemplater:
+    def __init__(self):
+        try:
+            self.proj_folder = argv[1]
+        except IndexError:
+            self.proj_folder = os.path.dirname(os.path.realpath(__file__))
+        self.pkg_dir = os.path.join(self.proj_folder, 'app_pkg')
+        self.templates_dir = os.path.join(self.pkg_dir, 'templates')
+        self.static_dir = os.path.join(self.pkg_dir, 'static')
+
+    def make_folders(self):
+        # create the main package folder
+        os.mkdir(self.pkg_dir)
+        # create templates folder
+        os.mkdir(self.templates_dir)
+        # create static folders
+        static_fols = ['css', 'js']
+        for fol in static_fols:
+            os.makedirs(os.path.join(self.static_dir, fol))
+        print('Created project folders...\n')
+
+    def make_run_file(self):
+        with open(os.path.join(self.proj_folder, 'run.py'), 'w') as file:
+            file.write('from app_pkg import app')
+
+    def make_init_file(self):
+        '''
+        Creates the app_pkg package __init__ file.
+        '''
+        with open(os.path.join(self.pkg_dir, '__init__.py'), 'w') as file:
+            file_content = '\
+from flask import Flask\n\
+#from flask_sqlalchemy import SQLAlchemy\n\
+#from flask_migration import Migration\n\
+#from flask_login import LoginManager\n\n\
+app = Flask(__name__)\n\
+# generate the secret key using os.urrandom(24) then pasting the result in here\n\
+#app.secret_key = \'<your-secret-key>\'\n\
+#app.config.from_pyfile(\'config.py\')\n\n\
+# remove below if not using a database\n\
+#db = SQLAlchemy(app)\n\
+#migrate = Migrate(app, db)\n\n\
+#lm = LoginManager()\n\
+#lm.login_view = \'login\'\n\
+#lm.init_app(app)\n\n\
+from app_pkg import views, models\n\
+'
+            file.write(file_content)
+        print('Created app __init__ file...\n')
+
+    def make_config_file(self):
+        '''
+        Creates the app configuration file. This file contains definitions for database connections, migrations, and other infrastructure type settings.
+        '''
+        with open(os.path.join(self.pkg_dir, 'config.py'), 'w') as file:
+            file_content = '\
+#import os\n\
+# uncomment below if using a database\n\
+#DB_URL = \'localhost:<db-port-number>\'\n\
+#DB_USER = \'<db-user-name>\'\n\
+#DB_PW = \'emergency\'\n\
+#DB_NAME = \'<database-name>\'\n\
+#DB = \'postgresql+psycopg2://{user}:{pw}@{url}/{db}\'.format(user=DB_USER, pw=DB_PW, url=DB_URL, db=DB_NAME)\n\
+#SQLALCHEMY_DATABASE_URI = DB or \'sqlite:///\' + os.path.join(basedir, \'app.db\')\n\
+# uncomment below if using flask-migrate for database migration\n\
+# define the database migration data files directory\n\
+#basedir = os.path.abspath(os.path.dirname(__file__))\n\
+#SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, \'db_repository\')\n\
+#SQLALCHEMY_TRACK_MODIFICATIONS = False\n\
+'
+            file.write(file_content)
+        print('Created configuration file...\n')
+
+    def make_forms_file(self):
+        '''
+        Create file to define form classes.
+        '''
+        with open(os.path.join(self.pkg_dir, 'forms.py'), 'w') as file:
+            file_content = '\
+#from flask_wtf import FlaskForm\n\
+#from wtforms import StringField, PasswordField, SubmitField\n\
+#from wtforms.validators import Email, DataRequired\n\n\
+#class SignInForm(FlaskForm):\n\
+#    email = StringField(\'email\', validators=[DataRequired(), Email()])\n\
+#    password = PasswordField(\'password\', validators=[DataRequired()])\n\
+#    submit = SubmitField("Sign In")\n\n\
+# define other classes...\
+'
+            file.write(file_content)
+        print('Created forms file...\n')
+
+    def make_models_file(self):
+        '''
+        Create the models file to define database object models.
+        '''
+        with open(os.path.join(self.pkg_dir, 'models.py'), 'w') as file:
+            file_content = '\
+#from app_pkg import db\n\
+# include below to use existing tables without having to define the schema\n\
+# db.Model.metadata.reflect(db.engine)\n\n\
+# define model classes...\n\
+#class <ClassName>(db.Model):\n\
+#    __table__ = db.Model.metadata.tables[\'<table-name>\']\n\
+#    def __repr__(self):\n\
+#    return \'<<repr-str> {}>\'.format(self.<some-field>)\n\
+'
+            file.write(file_content)
+        print('Created models file...\n')
+
+    def make_views_file(self):
+        '''
+        Create file to hold app views/routes.
+        '''
+
+        with open(os.path.join(self.pkg_dir, 'views.py'), 'w') as file:
+            file_content = '\
+# remove db and lm if not using database or login manager\n\
+from app_pkg import app, db, lm\n\
+from flask import render_template, flash, redirect, url_for, request\n\
+# uncomment to enable user authentication\n\
+#from flask_login import login_user, logout_user, current_user, login_required\n\
+#from .models import your, models\n\
+#from .forms import your, forms\n\n\
+# uncomment to enable user authentication\n\
+#@lm.user_loader\n\
+#def load_user(email):\n\
+#    return User.query.filter_by(email=email).first()\n\
+'
+            file.write(file_content)
+        print('Created views file...\n')
+
+    def make_template_file(self):
+        '''
+        Create intial template file.
+        '''
+        open(os.path.join(self.templates_dir, 'index.html'), 'w').close()
+        print('Created index template...\n')
+
+    def make_project(self):
+        self.make_folders()
+        self.make_run_file()
+        self.make_init_file()
+        self.make_config_file()
+        self.make_forms_file()
+        self.make_models_file()
+        self.make_views_file()
+        self.make_template_file()
+
+
+if __name__ == '__main__':
+    FT = FlaskTemplater()
+    FT.make_project()
